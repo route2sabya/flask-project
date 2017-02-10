92ae421492bd58c03b02c9c482a102099da96261
sabyasachi <sabyasachi_purkayastha@yahoo.co.in>
2017-02-10 23:27:31 +0530

    OMG

---
+++ static/data/5minconcavehull2.js
@@ -0,0 +1,7 @@
+var json_5minconcavehull2={
+"type": "FeatureCollection",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "0", "count": "8" }, "geometry": { "type": "Polygon", "coordinates": [ [ [ 77.165119, 28.583686 ], [ 77.1673465, 28.5926189 ], [ 77.1673885, 28.5908666 ], [ 77.170712599999987, 28.5860962 ], [ 77.168913, 28.585632 ], [ 77.165119, 28.583686 ] ] ] } }
+]
+}
